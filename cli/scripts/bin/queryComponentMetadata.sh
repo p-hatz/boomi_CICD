4672d61df0f2b561fb72f937cd8ece338cd34a3a
p-hatz <141098596+p-hatz@users.noreply.github.com>
2024-01-06 19:34:27 +1100

    Update queryComponentMetadata.sh

--- cli/scripts/bin/queryComponentMetadata.sh
+++ cli/scripts/bin/queryComponentMetadata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source $WD/bin/common.sh
+#source $WD/bin/common.sh
 
 unset _saveComponentId _saveComponentType _saveComponentName _saveComponentVersion exportVariable
 # mandatory arguments
