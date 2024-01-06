4f11eebeeb41f6541cd649c98edb1f21f2d347e2
p-hatz <141098596+p-hatz@users.noreply.github.com>
2024-01-06 18:58:53 +1100

    Update createPackage.sh

--- cli/scripts/bin/createPackage.sh
+++ cli/scripts/bin/createPackage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source $WD/bin/common.sh
+source ${GITHUB_WORKSPACE}/cli/bin/common.sh
+
+echo post $authToken > /tmp/post.out
 
 # mandatory arguments
 ARGUMENTS=(packageVersion notes) 
@@ -13,7 +15,7 @@ fi
 
 if [ ! -z "${extractComponentXmlFolder}" ]
 then
- folder="${WORKSPACE}/${extractComponentXmlFolder}"
+ folder="${GITHUB_WORKSPACE}/${extractComponentXmlFolder}"
  rm -rf ${folder}
  unset extensionJson
  saveExtractComponentXmlFolder="${extractComponentXmlFolder}"
@@ -22,7 +24,7 @@ fi
 saveNotes="${notes}"
 saveTag="${tag}"
 
-source $WD/bin/createSinglePackage.sh "$@"
+source $GITHUB_WORKSPACE/cli/bin/createSinglePackage.sh "$@"
 
 handleXmlComponents "${saveExtractComponentXmlFolder}" "${saveTag}" "${saveNotes}"
 
