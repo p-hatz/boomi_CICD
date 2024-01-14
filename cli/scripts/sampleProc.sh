df8311b9710f3143b53e74cc54ca610c7e2e6eb4
peterhatz <141098596+p-hatz@users.noreply.github.com>
2024-01-14 17:50:36 +1100

    Update sampleProc.sh

--- cli/scripts/sampleProc.sh
+++ cli/scripts/sampleProc.sh
@@ -3,4 +3,4 @@
 /bin/rm -f /tmp/$2.sample
 timeout $1s bash -c "while true ; do ps -Ao pid,pcpu | grep $(jps|grep Launcher|awk '{ print $1 }') | cut -f5 -d' ' >> /tmp/$2.sample ; sleep 3 ; done "
 
-spark < /tmp/$2.sample
+spark < /tmp/$2.sample > /tmp/$2.sample.out
