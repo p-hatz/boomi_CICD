63241bde4af0284e1989679434129a28a02e1ca9
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2024-02-28 13:07:29 +1100

    Update queryAtom.sh

--- cli/scripts/bin/queryAtom.sh
+++ cli/scripts/bin/queryAtom.sh
@@ -4,8 +4,9 @@ source ${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh
 
 # get atom id of the by atom name
 # mandatory arguments
-ARGUMENTS=(atomName atomType atomStatus)
-JSON_FILE=json/queryAtom.json
+#ARGUMENTS=(atomName atomType atomStatus)
+ARGUMENTS=(atomName atomStatus)
+JSON_FILE=$WD/json/queryAtom.json
 URL=$baseURL/Atom/query
 id=result[0].id
 exportVariable=atomId
@@ -19,7 +20,7 @@ fi
 
 if [ "$atomType" = "*" ] || [ "$atomStatus" = "*" ]
 then
-        JSON_FILE=json/queryAtomAny.json
+        JSON_FILE=$WD/json/queryAtomAny.json
 fi
 createJSON
  
