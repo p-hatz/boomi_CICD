d1b7748aea637e2e990df6c831ef51adcad6d17c
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2024-06-10 12:37:12 +1000

    Create initCheckPackage.sh

---
+++ cli/scripts/initCheckPackage.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -a
+
+source ${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh
+
+ARGUMENTS=(authToken componentId componentType packageVersion envId codeCheck)
+inputs "$@"
+if [ "$?" -gt 0 ]
+then
+	exit $?
+fi
+
+saveComponentId=${componentId}
+echo save1: $saveComponentId
+
+source ${GITHUB_WORKSPACE}/cli/scripts/bin/queryPackagedComponent.sh authToken=$authToken componentId=$componentId packageVersion=$packageVersion componentType=$componentType
+if [ -z "$packageId" ]
+then
+	echo "No package has been created for Component Id $componentId!"
+	exit 1
+fi
+
+echo save: $saveComponentId
+savePackageId=$packageId
+
+source ${GITHUB_WORKSPACE}/cli/scripts/bin/queryDeployedPackage.sh envId=$envId packageId=$packageId
+if [ -z "$deploymentId" ]
+then
+	echo "Package has been not been deployed to Dev"
+	exit 1
+fi
+
+echo save: $saveComponentId
+if [ $codeCheck -eq 1 ]
+then
+	echo Checking code!...
+        source "${GITHUB_WORKSPACE}/cli/scripts/initCheckRules.sh" packageId=$savePackageId componentId=$saveComponentId extractComponentXmlFolder=CodeReview
+fi          
