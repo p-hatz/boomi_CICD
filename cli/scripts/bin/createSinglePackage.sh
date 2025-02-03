eef31c112ffe186dba0d83ebd07515c5c5c4c148
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2025-02-04 08:22:11 +1100

    Update createSinglePackage.sh

--- cli/scripts/bin/createSinglePackage.sh
+++ cli/scripts/bin/createSinglePackage.sh
@@ -111,7 +111,8 @@ then
  	#export tag="${processName}"
 	#export notes="Created from GitHub Actions Pipeline"
 	#source $WD/bin/gitPush.sh "${notes}" "${tag}"
- 	_issueCount=$(curl -s --request GET --url http://pn50:9000/api/issues/search\?project\=boomi\&issueStatuses\=OPEN --header 'authorization: Basic c3F1X2NmOTczZTY0NTQ0OWEyYmM2ZDZmZTAzYTc4OWUzNDY3NWJhNmU5ZTI6' | jq -r ".total")
+ 	_url="${sonarURL}/api/issues/search\?project\=boomi\&issueStatuses\=OPEN --header 
+ 	_issueCount=$(curl -s --request GET --url ${sonarURL}/api/issues/search\?project\=boomi\&issueStatuses\=OPEN --header 'authorization: Basic c3F1X2NmOTczZTY0NTQ0OWEyYmM2ZDZmZTAzYTc4OWUzNDY3NWJhNmU5ZTI6' | jq -r ".total")
   	echo $_issueCount
     	if [ "$_issueCount" -gt 0 ]
      	then
