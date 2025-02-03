aa97a3e08d1a1fdddff6f9c31a787159bf50525c
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2025-02-04 08:16:02 +1100

    Update createSinglePackage.sh

--- cli/scripts/bin/createSinglePackage.sh
+++ cli/scripts/bin/createSinglePackage.sh
@@ -112,9 +112,10 @@ then
 	#export notes="Created from GitHub Actions Pipeline"
 	#source $WD/bin/gitPush.sh "${notes}" "${tag}"
  	_issueCount=$(curl -s --request GET --url http://pn50:9000/api/issues/search\?project\=boomi\&issueStatuses\=OPEN --header 'authorization: Basic c3F1X2NmOTczZTY0NTQ0OWEyYmM2ZDZmZTAzYTc4OWUzNDY3NWJhNmU5ZTI6' | jq -r ".total")
-  echo $_issueCount
-    	if [ "$_issueCount" != 0 ]
+  	echo $_issueCount
+    	if [ "$_issueCount" -gt 0 ]
      	then
+      		echo Issues found with scan!
       		return 255
 	fi
 fi
