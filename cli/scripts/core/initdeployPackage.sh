4fb1cd1eec97e101448f3a4f8ff8b2353c8393be
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2026-03-29 14:46:58 +1100

    Update initdeployPackage.sh

--- cli/scripts/core/initdeployPackage.sh
+++ cli/scripts/core/initdeployPackage.sh
@@ -2,7 +2,7 @@
 
 set -a
 
-source bin/common.sh
+source ${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh
 
 ARGUMENTS=(authToken componentId packageVersion notes componentType env listenerStatus updExt)
 
@@ -12,4 +12,4 @@ then
 	exit $?
 fi
 
-bin/deployPackage.sh componentId=$componentId packageVersion=$packageVersion componentType=$componentType notes="$notes" env=$env listenerStatus=$listenerStatus updExt=$updExt
+source ${GITHUB_WORKSPACE}/cli/scripts/bin/deployPackage.sh componentId=$componentId packageVersion=$packageVersion componentType=$componentType notes="$notes" env=$env listenerStatus=$listenerStatus updExt=$updExt
