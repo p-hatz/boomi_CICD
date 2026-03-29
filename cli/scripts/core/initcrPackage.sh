05c3486b993efa9fb70963a77c4577c794d14a1c
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2026-03-29 14:55:46 +1100

    Update initcrPackage.sh

--- cli/scripts/core/initcrPackage.sh
+++ cli/scripts/core/initcrPackage.sh
@@ -2,7 +2,7 @@
 
 set -a
 
-source ${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh
+source "${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh"
 
 ARGUMENTS=(authToken componentId packageVersion notes componentType extractComponentXmlFolder)
 
@@ -12,5 +12,5 @@ then
 	exit $?
 fi
 
-source ${GITHUB_WORKSPACE}/cli/scripts/bin/createPackage.sh authToken=$authToken componentId=$componentId packageVersion=$packageVersion componentType=$componentType notes="$notes" extractComponentXmlFolder="$extractComponentXmlFolder="
+source "${GITHUB_WORKSPACE}/cli/scripts/bin/createPackage.sh" authToken=$authToken componentId=$componentId packageVersion=$packageVersion componentType=$componentType notes="$notes" extractComponentXmlFolder="$extractComponentXmlFolder="
 exit $?
