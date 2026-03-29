7932ab8a3d6d99ff92afb5dd359e1866654e427c
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2026-03-29 14:54:08 +1100

    Update initCheckPackage.sh

--- cli/scripts/core/initCheckPackage.sh
+++ cli/scripts/core/initCheckPackage.sh
@@ -2,7 +2,7 @@
 
 set -a
 
-source ${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh
+source "${GITHUB_WORKSPACE}/cli/scripts/bin/common.sh"
 
 ARGUMENTS=(authToken componentId componentType packageVersion envId codeCheck checkDeployment)
 inputs "$@"
@@ -14,7 +14,7 @@ fi
 saveComponentId=${componentId}
 echo save1: $saveComponentId
 
-source ${GITHUB_WORKSPACE}/cli/scripts/bin/queryPackagedComponent.sh authToken=$authToken componentId=$componentId packageVersion=$packageVersion componentType=$componentType
+source "${GITHUB_WORKSPACE}/cli/scripts/bin/queryPackagedComponent.sh" authToken=$authToken componentId=$componentId packageVersion=$packageVersion componentType=$componentType
 if [ -z "$packageId" ]
 then
 	echo "No package has been created for Component Id $componentId!"
@@ -28,7 +28,7 @@ if [ $checkDeployment -eq 0 ]
 then
 	echo "Not checking for deployment"
 else
-	source ${GITHUB_WORKSPACE}/cli/scripts/bin/queryDeployedPackage.sh envId=$envId packageId=$packageId
+	source "${GITHUB_WORKSPACE}/cli/scripts/bin/queryDeployedPackage.sh" envId=$envId packageId=$packageId
 	echo deploymentid: $deploymentId
 	if [ -z "$deploymentId" ] || [ "$deploymentId" = "null" ]
 	then
@@ -41,5 +41,5 @@ echo save: $saveComponentId
 if [ $codeCheck -eq 1 ]
 then
 	echo Checking code!...
-    source ${GITHUB_WORKSPACE}/cli/scripts/bin/initCheckRules.sh" packageId=$savePackageId componentId=$saveComponentId extractComponentXmlFolder=CodeReview
+    source "${GITHUB_WORKSPACE}/cli/scripts/bin/initCheckRules.sh" packageId=$savePackageId componentId=$saveComponentId extractComponentXmlFolder=CodeReview
 fi          
